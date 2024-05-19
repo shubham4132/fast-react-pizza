726e9847eaddad73b2063e815fd7fc8b4335ee70
shubham4132 <shubhamg3122134@gmail.com>
2024-05-19 11:59:14 +0530

    Learning The Box Model border padding spacing margin using tailwind

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,8 +1,8 @@
 import { Link } from 'react-router-dom'
 function CartOverview() {
     return (
-        <div className="bg-stone-800 uppercase text-stone-200">
-            <p className="text-stone-300">
+        <div className="bg-stone-800 p-4 uppercase text-stone-200">
+            <p className="space-x-4 text-stone-300">
                 <span className="font-semibold">23 pizzas</span>
                 <span>$23.45</span>
             </p>
