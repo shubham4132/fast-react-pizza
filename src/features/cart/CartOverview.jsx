ea4b766296761baf715fcd4eb65747ec714ddf49
shubham4132 <shubhamg3122134@gmail.com>
2024-05-21 22:13:51 +0530

    Using FlexBox

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 function CartOverview() {
     return (
-        <div className="bg-stone-800 p-4 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
+        <div className="item-center flex justify-between  bg-stone-800 p-4 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
             <p className="text-stone-300 sm:space-x-4 ">
                 <span className="font-semibold">23 pizzas</span>
                 <span>$23.45</span>
